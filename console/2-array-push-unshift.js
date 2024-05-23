1b379dd07a257d47ce58d6b064c6b1626d663d7e
Fermento0 <evan.cordeiro@escola.pr.gov.br>
2024-05-23 11:11:46 -0300

    h

---
+++ console/2-array-push-unshift.js
@@ -0,0 +1,12 @@
+var frutas = [];
+
+frutas.push('maça');
+frutas.push('Banana');
+frutas.push('Laranja');
+
+console.log('Array após adicionar elementos ao final:', frutas);
+
+frutas.unshift('Pêssego');
+frutas.unshift('Abacaxi');
+
+console.log('Array após adicionar elementos ao inicio:', frutas);
---
+++ console/3-array-pop-shift.js
@@ -0,0 +1,10 @@
+var frutas = ['Maçã', 'Banana', 'Laranja', 'Pêssego', 'Abacaxi',];
+
+var ultimaFruta = frutas.pop();
+
+console.log('Array após remover o último elemento:', frutas);
+console.log('Última fruta removida:', ultimaFruta);
+
+var primeiraFruta = frutas.shift();
+console.log('Array após remover o primeiro elemento:', frutas);
+console.log('Primeira fruta removida:', primeiraFruta);
