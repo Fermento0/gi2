eab6d4c0005634977c79c81ec3905fb810440f3c
Fermento0 <evan.cordeiro@escola.pr.gov.br>
2024-06-13 11:52:47 -0300

    aksfj

---
+++ console/concatear-array-e-dados.js
@@ -0,0 +1,9 @@
+const arrayOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const arrayConcat = arrayOriginal.concat("André", "Fernanda")
+console.log(arrayOriginal)
+console.log(arrayConcat)
+
+const arrayOriginal_2 = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const arrayConcat2 = arrayOriginal_2.concat(["André", "Fernanda"], ["Ricardo", "Ana"], ["Marcelo", "Bia"])
+console.log(arrayOriginal_2)
+console.log(arrayConcat2)
